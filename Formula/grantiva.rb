1ad1ff77cafe3a3f592087e76df1d1827638660c
Kyle Browning <kylebrowning@me.com>
2026-03-30 21:00:26 -0700

    Update grantiva to 0.7.3

--- Formula/grantiva.rb
+++ Formula/grantiva.rb
@@ -1,9 +1,9 @@
 class Grantiva < Formula
   desc "The CLI for iOS developers — visual regression testing, feature flags, and more"
   homepage "https://grantiva.io"
-  url "https://github.com/grantiva/cli/releases/download/0.7.0/grantiva-0.7.0-macos.tar.gz"
-  sha256 "bbb2125c978eea6bea3071177a45020bb256e5699886b3db6e67ca9d55b920aa"
-  version "0.7.0"
+  url "https://github.com/grantiva/cli/releases/download/0.7.3/grantiva-0.7.3-macos.tar.gz"
+  sha256 "39303eb14b2faa5af205ef7437494dcc6b146fc71a0994b9b8c0069dff384ea6"
+  version "0.7.3"
   license "MIT"
 
   depends_on :macos
